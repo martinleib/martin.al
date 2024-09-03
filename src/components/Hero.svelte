7ad8b033f99d2fee70738ab9fdabf9bd0be8d1d3
martinleib <m@martinleib.uy>
2024-09-03 18:59:19 -0300

    portfolio v0.1

---
+++ src/components/Hero.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Arrow from '../icons/Arrow.svelte';
+</script>
+
+<section class="text-zinc-200 items-start mt-5">
+    <h1>Martin Leib</h1>
+    <h2 class="mt-5 mb-5 sm:mb-0">
+        I'm a front-end developer located in <span class="serif"
+            >Piriápolis, Uruguay</span
+        >. I'm driven by creating seamless web experiences.
+    </h2>
+    <h2>
+        I'm currently studying Systems Analysis at Universidad ORT in
+        <span class="serif">Montevideo, Uruguay</span>.
+    </h2>
+    <h2 class="mt-5">
+        Past clients include <a
+            href="https://davusito.com"
+            class="underline underline-offset-2 decoration-1 hover:text-rose-400 duration-200">Davus</a
+        ><Arrow />
+    </h2>
+</section>
---
+++ src/components/Skills.svelte
@@ -0,0 +1,14 @@
+<section class="text-zinc-200 pt-5 mt-8">
+    <h4 class="text-zinc-400 text-sm mb-3">Languages</h4>
+    <p class="mt-2">Spanish</p>
+    <p class="text-zinc-400 text-xs">native</p>
+    <p class="mt-2">English</p>
+    <p class="text-zinc-400 text-xs">C2 Proficiency</p>
+    <p class="mt-2">Portuguese</p>
+    <p class="text-zinc-400 text-xs">B1 Intermediate</p>
+</section>
+
+<section class="text-zinc-200 pt-5 mt-8">
+    <h4 class="text-zinc-400 text-sm mb-3">Skills</h4>
+    <p class="mt-2">HTML, CSS, JavaScript, Tailwind, Svelte, Astro, Git</p>
+</section>
